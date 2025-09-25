6353e1d63ccfade4b7d9ac96fcd42daac5824617
allxn753 <mailtoallenmathew@gmail.com>
2025-09-25 14:28:31 -0400

    intial switch testing

--- disconnect_all.py
+++ disconnect_all.py
@@ -1,17 +1,13 @@
 from CryoSwitchController.CryoSwitchController import Cryoswitch
 
-# Initialize
+
 switch = Cryoswitch()
 
-# Start controller hardware
 switch.start()   # enables rails
 
 # Set voltage and pulse duration
 switch.set_output_voltage(25)
 switch.set_pulse_duration_ms(15)
 
-# Switch actuator contact
-switch.connect(port='A', contact=1)
-
-
-
+for i in range(1,7):
+    switch.disconnect(port='A', contact=i)
